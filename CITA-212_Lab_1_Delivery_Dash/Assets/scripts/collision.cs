7ee45e18a258098348b22ef06de7809c48c2650f
leqking <bajablast07@gmail.com>
2026-09-22 22:48:53 -0400

    collision sprites etc

--- CITA-212_Lab_1_Delivery_Dash/Assets/scripts/collision.cs
+++ CITA-212_Lab_1_Delivery_Dash/Assets/scripts/collision.cs
@@ -5,7 +5,12 @@ public class collision : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Ouch");
+    }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("What was that");
     }
 
 }
