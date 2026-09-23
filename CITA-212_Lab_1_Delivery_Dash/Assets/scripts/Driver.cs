1453a258450548f1ba2995380ff82b90f831f37b
leqking <bajablast07@gmail.com>
2026-09-22 21:23:14 -0400

    movement and collision

--- CITA-212_Lab_1_Delivery_Dash/Assets/scripts/Driver.cs
+++ CITA-212_Lab_1_Delivery_Dash/Assets/scripts/Driver.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.1f;
+   
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 1f);
-        transform.Translate(0, 0.1f, 0);
+
+        float steer = 0f;
+        float move = 0f;
+
+
+
+        if(Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
 
     }
 }
---
+++ CITA-212_Lab_1_Delivery_Dash/Assets/scripts/collision.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+}
